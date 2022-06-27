9526ab2fd9637f737a277a896fd823f8037da506
SimonFJ20 <simonfromjakobsen@gmail.com>
2022-06-27 03:22:36 +0200

    fisix sorta works

--- v0/lib/state.dart
+++ v0/lib/state.dart
@@ -1,6 +1,9 @@
 import 'dart:collection';
 import 'dart:math' as math;
 
+import 'package:app_v0/vector2d.dart';
+import 'package:flutter/gestures.dart';
+
 abstract class StateManagable {
   void refreshState();
 }
@@ -20,6 +23,11 @@ class StateManager {
   late double _rocketY = 0.0;
   late double _angleDegree = -90.0;
 
+  late Vector2d position = Vector2d(0, 0);
+  late Vector2d positionalVelocity = Vector2d(0, 0);
+  late double angle = 0.0;
+  late double angularVelocity = 0.0;
+
   StateManager({required StateManagable state}) {
     _state = state;
   }
@@ -33,10 +41,9 @@ class StateManager {
   }
 
   bool get rocketMoving {
-    return _buttonsDown[RocketAction.left] == true ||
-        _buttonsDown[RocketAction.forward] == true ||
-        _buttonsDown[RocketAction.right] == true ||
-        false;
+    return _buttonsDown[RocketAction.left]! ||
+        _buttonsDown[RocketAction.forward]! ||
+        _buttonsDown[RocketAction.right]!;
   }
 
   double get rocketX {
@@ -51,23 +58,65 @@ class StateManager {
     return _angleDegree * (math.pi / 180);
   }
 
+  // void tick(double deltaT) {
+  //   if (_buttonsDown[RocketAction.left] == true) {
+  //     _angleDegree--;
+  //   }
+
+  //   if (_buttonsDown[RocketAction.right] == true) {
+  //     _angleDegree++;
+  //   }
+
+  //   if (_buttonsDown[RocketAction.forward] == true) {
+  //     double x = math.cos(this.angleRadian);
+  //     double y = math.sin(this.angleRadian);
+
+  //     _rocketX += x;
+  //     _rocketY += y;
+  //   }
+
+  //   _state.refreshState();
+  // }
+
+  double thrustAngle() {
+    const angle = (5 / 180) * math.pi;
+    return _buttonsDown[RocketAction.left]! &&
+            !_buttonsDown[RocketAction.right]!
+        ? angle
+        : _buttonsDown[RocketAction.right]! && !_buttonsDown[RocketAction.left]!
+            ? -angle
+            : 0;
+  }
+
+  double calculateCylinderMomentOfIntertia(
+          double height, double radius, double mass) =>
+      (1 / 12) * mass * (3 * math.pow(radius, 2) + math.pow(height, 2));
+
+  double calculateThrustTorque(
+          double absoluteForce, double radius, double angle) =>
+      radius * absoluteForce * math.sin(angle);
+
+  Vector2d calculateThrustForce(double rocketAngle, double engineRadius,
+          double absoluteForce, double engineAngle) =>
+      Vector2d(math.sin(rocketAngle + math.pi), math.cos(rocketAngle + math.pi))
+          .multiplyN(absoluteForce);
+
   void tick(double deltaT) {
-    if (_buttonsDown[RocketAction.left] == true) {
-      _angleDegree--;
-    }
-
-    if (_buttonsDown[RocketAction.right] == true) {
-      _angleDegree++;
-    }
-
-    if (_buttonsDown[RocketAction.forward] == true) {
-      double x = math.cos(this.angleRadian);
-      double y = math.sin(this.angleRadian);
-
-      _rocketX += x;
-      _rocketY += y;
-    }
-
+    var thrustTorque = calculateThrustTorque(5000, 5, thrustAngle());
+    var resultingTorque = thrustTorque;
+    var momentOfInteria = calculateCylinderMomentOfIntertia(10, 5, 500);
+    var rotationalAcceleration = (resultingTorque / momentOfInteria) * deltaT;
+    angularVelocity += rotationalAcceleration;
+    angle += angularVelocity * deltaT;
+    var thrustForce = calculateThrustForce(angle, 5, 5000, thrustAngle());
+    var resultingForce = rocketMoving ? thrustForce : Vector2d(0, 0);
+    var transitionalAcceleration =
+        resultingForce.divideN(500).multiplyN(deltaT);
+    positionalVelocity += transitionalAcceleration;
+    position += positionalVelocity.copy().multiplyN(deltaT);
+    _rocketX = position.x;
+    _rocketY = position.y;
+    _angleDegree = -(angle / math.pi) * 180 - 90;
     _state.refreshState();
   }
 }
